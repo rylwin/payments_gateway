a9bc254e46ed4af3991c63726a81776e3d3dacba
Ryan Winograd <ryan@thewinograds.com>
2012-05-03 09:03:17 -0500

    Require attributes.rb by full path

--- lib/payments_gateway.rb
+++ lib/payments_gateway.rb
@@ -2,11 +2,10 @@ require 'openssl'
 require 'soap/wsdlDriver'
 require 'active_support/all'
 
-require 'lib/payments_gateway/attributes' # must be required first
-
 module PaymentsGateway
   dir = File.dirname(__FILE__) + '/payments_gateway'
   $LOAD_PATH.unshift(dir)
+  require dir + '/attributes.rb' # must be required first
   Dir[File.join(dir, "*.rb")].each {|file| require File.basename(file) }
 end
 
